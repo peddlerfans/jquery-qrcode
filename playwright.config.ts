effd2b332167cda3a784a86a7bd28bf3c28fe2a4
Peter Peng <peter.peng1215@gmail.com>
2022-09-14 03:46:24 +0000

    Functional testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,27 +10,28 @@ const config: PlaywrightTestConfig = {
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
         },
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
         // "iPhone 13 Pro" tests use WebKit browser.
-        {
-            name: 'iPhone 13 Pro',
-            use: {
-                browserName: 'webkit',
-                ...devices['iPhone 13 Pro'],
-            }
-        },
-        // "Pixel 4 landscape" tests use Chromium browser.
-        {
-            name: 'Pixel 4 landscape',
-            use: {
-              browserName: 'chromium',
-              ...devices['Pixel 4 landscape'],
-            },
-        },
+        // {
+        //     name: 'iPhone 13 Pro',
+        //     use: {
+        //         browserName: 'webkit',
+        //         ...devices['iPhone 13 Pro'],
+        //     }
+        // },
+        // // "Pixel 4 landscape" tests use Chromium browser.
+        // {
+        //     name: 'Pixel 4 landscape',
+        //     use: {
+        //       browserName: 'chromium',
+        //       ...devices['Pixel 4 landscape'],
+        //     },
+        // },
     ],
+    testIgnore:["**/__tests__/**"],
     reporter: [['line'], ['allure-playwright']]
 };
 
