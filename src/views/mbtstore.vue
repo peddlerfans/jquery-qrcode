45bf0e9898bd3eb932df9c2eb7562ce6ffd23f4e
Edwin <edwin@iteatechnologies.com>
2022-09-15 17:37:06 +0000

    更新 src/views/mbtstore.vue

--- src/views/mbtstore.vue
+++ src/views/mbtstore.vue
@@ -5,7 +5,7 @@ import { message } from 'ant-design-vue/es'
 import * as _ from 'lodash'
 import { ref, reactive, computed, onBeforeMount, defineComponent, UnwrapRef, onMounted, nextTick, watch ,getCurrentInstance} from 'vue';
 import type { FormProps, SelectProps, TableProps, TreeProps } from 'ant-design-vue';
-import {tableSearch  FormState, ModelState, statesTs } from "./componentTS/mbtmodeler";
+import {tableSearch , FormState, ModelState, statesTs } from "./componentTS/mbtmodeler";
 import { Rule } from 'ant-design-vue/es/form';
 import {  PlusOutlined,  EditOutlined,} from '@ant-design/icons-vue';
 
