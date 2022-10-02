c4de71e518df83b65c4e7ad29fad701988b10b7b
familydai7 <familydai7@gmail.com>
2022-10-02 16:16:59 +0800

    Add new static template and modeler

--- src/views/statictemplate.vue
+++ src/views/statictemplate.vue
@@ -1,132 +1,316 @@
 <script setup lang="ts">
-import { ref, reactive, computed, onBeforeMount } from 'vue';
-import type { TreeProps } from 'ant-design-vue';
-import {
-  SyncOutlined,
-  PlusOutlined,
-  EditOutlined,
-  DeleteOutlined,
-  ExclamationCircleOutlined,
-  SwapOutlined,
-} from '@ant-design/icons-vue';
-import { Tree, Dropdown, Space, Tooltip, Modal, Alert, Menu } from 'ant-design-vue';
-// import type { LoadDataParams } from '@/components/core/dynamic-table';
-// import type { TreeDataItem } from '@/core/permission/utils';
-import { SplitPanel } from '@/components/basic/split-panel';
+import { ref, reactive, computed, onBeforeMount, UnwrapRef, onMounted, nextTick, getCurrentInstance } from 'vue';
+import { FormProps, message, SelectProps, TreeProps } from 'ant-design-vue';
+import { SyncOutlined, PlusOutlined, DeleteOutlined } from '@ant-design/icons-vue';
 import request from "@/utils/request"
-
-interface State {
-  expandedKeys: number[];
-  // departmentIds: number[];
-  // deptTree: TreeDataItem[];
-}
-
-// const [DynamicTable, dynamicTableInstance] = useTable();
-// const [showModal] = useFormModal();
-
-// const deptListLoading = ref(false);
-
-const state = reactive<State>({
-  expandedKeys: [],
-  // departmentIds: [],
-  // deptTree: [],
+import { tableSearch, FormState, statesTs, ModelState } from './componentTS/metatemplate';
+import cloneDeep from 'lodash-es/cloneDeep';
+import { useRouter, onBeforeRouteLeave } from 'vue-router';
+// import { FormState } from './componentTS/awmodeler';
+// 表单查询的数据
+const formState: UnwrapRef<FormState> = reactive({
+  search: '',
+  q: 'category:static'
 });
-
-
-const rowSelection = ref({
-  selectedRowKeys: [] as number[],
-  // onChange: (selectedRowKeys: number[], selectedRows: TableListItem[]) => {
-  //   console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-  //   rowSelection.value.selectedRowKeys = selectedRowKeys;
-  // },
-});
-
-
-/**
- * 获取statictemplate列表
- */
-const fetchStaticTemplate = async () => {
-  
+// 表单完成后的回调
+const handleFinish: FormProps['onFinish'] = async (values: any) => {
+  query(formState)
 };
+// 表单失败后的回调
+const handleFinishFailed: FormProps['onFinishFailed'] = (errors: any) => {
+  console.log(errors);
+};
+// 表格的数据
+let tableData = ref<Array<any>>([])
+let searchobj: tableSearch = reactive({
+  search: "",
+  // page: 1,
+  // perPage:10,
+  q: 'category:static'
+})
+const arr = (dataArr: any) => dataArr.map((item: any, index: string) => ({ ...item, key: index }))
+async function query(data?: any) {
+  let rst = await request.get("/api/templates", { params: data || searchobj })
+  console.log(rst.data);
 
+  tableData.value = arr(rst.data)
+}
+onMounted(() => {
+  query()
+})
+// 分页的数据
+let pagination = ref({
+  pageNo: 1,
+  pageSize: 10, // 默认每页显示数量
+  showQuickJumper: true,
+  showSizeChanger: true, // 显示可改变每页数量
+  pageSizeOptions: ['10', '20', '50', '100'], // 每页数量选项
+  showTotal: (total: any) => `共 ${total} 条`, // 显示总数
+  onShowSizeChange: (current: any, pageSize: any) => onSizeChange(current, pageSize), // 改变每页数量时更新显示
+  onChange: (page: any, pageSize: any) => onPageChange(page, pageSize),//点击页码事件
+  total: 0 //总条数
+})
 
-const expandedKeys = ref<string[]>(['0-0-0', '0-0-1']);
+const onPageChange = async (page: number, pageSize: any) => {
+  pagination.value.pageNo = page
+  pagination.value.pageSize = pageSize
+  // searchobj.page= page
+  // searchobj.perPage = pageSize
+  if (formState.search) {
+    searchobj.search = formState.search
+  } else {
+    searchobj.search = ''
+  }
+  await query()
+}
+//  分页的数据
+const onSizeChange = async (current: any, pageSize: number) => {
+  pagination.value.pageNo = current
+  pagination.value.pageSize = pageSize
+  //  searchobj.page= current
+  // searchobj.perPage = pageSize
+  if (formState.search) {
+    searchobj.search = formState.search
+  } else {
+    searchobj.search = ''
+  }
+  await query()
+}
+const expend = (isExpand: any, rected: any) => {
+  console.log(isExpand, rected);
 
-const treeData: TreeProps['treeData'] = [
-  {
-    title: 'parent 1',
-    key: '0-0',
-    children: [
-      {
-        title: 'parent 1-0',
-        key: '0-0-0',
-        disabled: true,
-        children: [
-          { title: 'leaf', key: '0-0-0-0', disableCheckbox: true },
-          { title: 'leaf', key: '0-0-0-1' },
-        ],
-      },
-      {
-        title: 'parent 1-1',
-        key: '0-0-1',
-        children: [{ key: '0-0-1-0', title: 'sss' }],
-      },
-    ],
-  },
-];
+}
+interface DataItem {
+  key?: string;
+  name: string;
+  description: string;
+  tags: Array<string>;
+  
+}
+const editableData: UnwrapRef<Record<string, DataItem>> = reactive({});
+// 点击修改static的方法
+const updstatic = async (data: any) => {
+  let rst = await request.put(`/api/templates/${data._id}`, data)
+  console.log(rst);
+}
+// 点击Edit触发的函数
+const edit = (key: string) => {
+  console.log(editableData);
 
+  editableData[key] = cloneDeep(tableData.value.filter((item: { key: string; }) => key === item.key)[0]);
+  console.log(editableData[key]);
+
+  states.tags = editableData[key].tags
+
+};
+// 点击save触发的函数
+const save = async (obj: any) => {
+  // Object.assign(tableData.value.filter((item: { key: string; }) => obj.key === item.key)[0], editableData[obj.key]);
+  editableData[obj.key].tags = states.tags
+  delete editableData[obj.key].key
+  console.log(editableData[obj.key]);
+
+  if (obj._id) {
+    await updstatic(editableData[obj.key])
+  } else {
+    await request.post("/api/templates", editableData[obj.key])
+  }
+  await query()
+  console.log(editableData[obj.key]);
+  delete editableData[obj.key];
+
+}
+const createstatic = () => {
+  const newstatic = {
+    key: tableData.value.length,
+    name: 'New template',
+    description: 'New template',
+    category: 'static',
+    tags: []
+    
+  }
+  tableData.value.push({ ...newstatic })
+}
+// 点击删除的方法
+const cancel = async (obj: any) => {
+  if (obj._id) {
+    let rst = await request.delete(`/api/templates/${obj._id}`)
+  }
+  delete editableData[obj.key];
+  message.success('Template deleted successfully')
+  query()
+
+};
+// 表格的结构
+const columns = reactive<Object[]>(
+  [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+
+    {
+      title: 'tags',
+      dataIndex: 'tags',
+      key: 'tags'
+    },
+
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+    }]
+)
+// 获取新建tags的dom
+let inputRef = ref();
+// 添加的表单tags
+let states = reactive<statesTs>({
+  tags: [],
+  inputVisible: false,
+  inputValue: '',
+});
+// 点击添加标签的方法
+const showInput = () => {
+  states.inputVisible = true;
+  nextTick(() => {
+    inputRef.value.focus();
+  })
+};
+// tag标签失去焦点之后添加的tags
+const handleInputConfirm = () => {
+  let tags = states.tags;
+  if (states.inputValue && tags.indexOf(states.inputValue) === -1) {
+    tags = [...tags, states.inputValue.toUpperCase()];
+  }
+  Object.assign(states, {
+    tags,
+    inputVisible: false,
+    inputValue: '',
+  });
+  console.log(states.tags);
+
+}
+// 移除tags
+const handleClose = (removedTag: string) => {
+  const tags = states.tags.filter((tag: string) => tag !== removedTag);
+  states.tags = tags;
+};
+  // onBeforeRouteLeave(async (to,form)=>{
+  // console.log("to", to , "form", form);
+  // if(to.path==`/staticModeler/${to.params._id}`){
+  //   let rst=await request.get(`/api/templates/${to.params._id}`,{params:{category:'static'}})
+  //   to.static.title=`staticModel ${rst.name}`
+  // }
+  // })
 </script>
-
+  
 <template>
-  <main class="main">
-    <!-- <section class="block shadow flex-center"
-      style="width: 100%; min-height: 100%; color: var(--gray); font-size: 5rem;"> -->
-      <SplitPanel>
-        <template #left-content>
-          <div class="flex justify-between">
-            <!-- <div>
-              <p style="height:16px!important;">组织1架构</p>
-            </div> -->
-            <Space>
-              <Tooltip v-if="true" placement="top">
-                <template #title>新增部门 </template>
-                <!-- <PlusOutlined @click="openDeptModal({})" /> -->
-              </Tooltip>
-              <Tooltip placement="top">
-                <template #title>刷新 </template>
-                <!-- <SyncOutlined :spin="deptListLoading"  /> -->
-              </Tooltip>
-            </Space>
-          </div>
-          <Tree v-model:expandedKeys="expandedKeys" auto-expand-parent :tree-data="treeData">
-            <template #title="{ key, title, formData }">
-              <Dropdown :trigger="['contextmenu']">
-                <span>{{ title }}</span>
-                <template #overlay>
-                  <Menu>
-                    <Menu.Item key="1">
-                      编辑
-                      <EditOutlined />
-                    </Menu.Item>
-                    <!-- <Menu.Item key="2" :disabled="!$auth('sys.dept.delete')" @click="delDept(key)">
-                  删除 <DeleteOutlined />
-                </Menu.Item> -->
-                  </Menu>
-                </template>
-              </Dropdown>
+  <main style="height:100%;overflow-x: hidden!important;">
+    <header class="block shadow" style="width:100%;margin-bottom: 1rem">
+      <a-row>
+        <a-col :span="20">
+          <AForm layout="inline" class="search_form" :model="formState" @finish="handleFinish"
+            @finishFailed="handleFinishFailed" :wrapper-col="{ span: 24 }">
+            <a-col :span="20">
+
+              <a-mentions v-model:value="formState.search" split=""
+                placeholder="input @ to search tags, input name to search MBT">
+                <a-mentions-option value="tags:">
+                  tags:
+                </a-mentions-option>
+              </a-mentions>
+            </a-col>
+
+            <a-col :span="4">
+              <a-button type="primary" html-type="submit">search</a-button>
+            </a-col>
+          </AForm>
+        </a-col>
+        <a-col :span="4">
+          <a-button type="primary" @click="createstatic">
+            <template #icon>
+              <plus-outlined />
             </template>
-          </Tree>
+          </a-button>
+        </a-col>
+      </a-row>
+    </header>
+
+    <a-table :columns="columns" :data-source="tableData" bordered>
+      <template #bodyCell="{ column, text, record }">
+        <template v-if='column.key==="name"'>
+          <div>
+            <a-input v-if="editableData[record.key]" v-model:value="editableData[record.key].name"
+              style="margin: -5px 0" />
+            <template v-else>
+              <a :href="'/#/staticModeler/'+record._id+'/'+record.name">{{text}}</a>
+              <!-- <router-link :to="{path:'/staticModeler',query:{record}}">{{ text }}</router-link> -->
+            </template>
+          </div>
         </template>
-        <template #right-content>
-          <!-- <p>
-            Show the details of organization
-          </p> -->
+        <template v-if='column.key==="description"'>
+          <div>
+            <a-input v-if="editableData[record.key]" v-model:value="editableData[record.key].description"
+              style="margin: -5px 0" />
+            <template v-else>
+              {{ text }}
+            </template>
+          </div>
         </template>
-      </SplitPanel>
-    <!-- </section> -->
+        <template v-if="column.key === 'tags'">
+          <template v-if="editableData[record.key]">
+            <template v-for="(tag, index) in states.tags" :key="tag">
+              <a-tooltip v-if="tag.length > 20" :title="tag">
+                <a-tag :closable="true" @close="handleClose(tag)">
+                  {{ `${tag.slice(0, 20)}...` }}
+                </a-tag>
+              </a-tooltip>
+              <a-tag v-else-if="tag.length==0"></a-tag>
+              <a-tag v-else :closable="true" @close="handleClose(tag)">
+                {{tag}}
+              </a-tag>
+            </template>
+            <a-input v-if="states.inputVisible" ref="inputRef" v-model:value="states.inputValue" type="text"
+              size="small" :style="{ width: '78px' }" @blur="handleInputConfirm" @keyup.enter="handleInputConfirm" />
+            <a-tag v-else style="background: #fff; border-style: dashed" @click="showInput">
+              <plus-outlined />
+              New Tag
+            </a-tag>
+          </template>
+          <span v-else>
+            <a-tag v-for="tag in record.tags" :key="tag" :color="tag === 'test' ? 'volcano' : 'red'">
+              {{ tag.toUpperCase() }}
+            </a-tag>
+          </span>
+        </template>
+
+        <template v-else-if="column.dataIndex === 'action'">
+          <div class="editable-row-operations">
+            <span v-if="editableData[record.key]">
+              <a-typography-link @click="save(record)">Save</a-typography-link>
+              <a-popconfirm title="Sure to cancel?" @confirm="cancel(record)">
+                <a style="margin-left:10px;font-size:14px">
+                  <delete-outlined />
+                </a>
+              </a-popconfirm>
+            </span>
+            <span v-else>
+              <a @click="edit(record.key)">Edit</a>
+            </span>
+          </div>
+        </template>
+      </template>
+    </a-table>
+
   </main>
 </template>
-
+  
 <style scoped lang="postcss">
 .main {
   position: relative;
---
+++ src/views/statictemplateModeler.vue
@@ -0,0 +1,355 @@
+<script setup lang="ts">
+import { nextTick, onMounted, reactive, ref, UnwrapRef, Ref, computed } from 'vue';
+import { onBeforeRouteLeave, useRoute } from 'vue-router';
+import request from "@/utils/request"
+import { cloneDeep } from 'lodash-es';
+import { message, SelectProps } from 'ant-design-vue';
+import { PlusOutlined, PlusSquareFilled, DeleteOutlined, SmileOutlined } from '@ant-design/icons-vue'
+import { object } from 'vue-types';
+import * as _ from 'lodash';
+let route = useRoute()
+console.log(route);
+
+interface TableDataItem {
+  key: string,
+  name?: string,
+  contacttype?: number,
+  msg?: string,
+  terminator?: string,
+  phonenum?: string
+}
+interface ColumnItem {
+  title: string,
+  dataIndex: string,
+  key: string
+}
+const dynamiccolumns: Ref<ColumnItem[]> = ref([{
+  title: 'ID',
+  dataIndex: 'name',
+  key: 'name',
+},
+{
+  title: 'ContactType',
+  dataIndex: 'contacttype',
+  key: 'contacttype',
+},
+{
+  title: 'MSG',
+  dataIndex: 'msg',
+  key: 'msg',
+},
+{
+  title: 'Terminatior',
+  dataIndex: 'terminator',
+  key: 'terminator',
+},
+{
+  title: 'PhoneNum',
+  dataIndex: 'phonenum',
+  key: 'phonenum',
+},
+{
+  title: 'action',
+  dataIndex: 'action',
+  key: 'action',
+  fixed:'right',
+  width: 100
+}
+])
+const tableData: Ref<any[]> = ref([
+
+  {
+    key: '1',
+    name: '1',
+    contacttype: 'real contact',
+    msg: '{{DS.test}}',
+    terminator: '{{phone2}}',
+    phonenum: '{{phone2.num}}',
+    test:'test'
+  },
+  {
+    key: '2',
+    name: '2',
+    contacttype: 'system contact',
+    msg: '{{DS.test}}',
+    terminator: '{{phone3}}',
+    phonenum: '{{phone4.num}}',
+    test:'test'
+  },
+]);
+
+
+const tablecolcount = computed(() => dynamiccolumns.value.length + 1);
+const tablecount = computed(() => tableData.value.length + 1);
+const staticeditableData: UnwrapRef<Record<string, TableDataItem>> = reactive({});
+
+const tableDataedit = (key: string) => {
+  staticeditableData[key] = cloneDeep(tableData.value.filter(item => key === item.key)[0]);
+};
+// const tableDatasave = (key: string) => {
+//   Object.assign(tableData.value.filter(item => key === item.key)[0], static[key]);
+//   delete static[key];
+// };
+
+const ontableDataDelete = (key: string) => {
+  tableData.value = tableData.value.filter(item => item.key !== key);
+};
+const tableDatahandleAdd = () => {
+  const newData = {
+    key: `${tablecount.value}`,
+    name: `Objective${tablecount.value}`,
+    age: tablecount.value,
+    address: `details${tablecount.value}`,
+  };
+  tableData.value.push(newData);
+};
+
+const columnshandleAdd = () => {
+  const newcolumn = {
+    title: `Property${tablecolcount.value}`,
+    dataIndex: `Property${tablecolcount.value}`,
+    key: `Property${tablecolcount.value}`
+
+  };
+  const actionColumn= {
+  title: 'action',
+  dataIndex: 'action',
+  key: 'action',
+  fixed:'right',
+  width: 100
+};
+  //  _.without(dynamiccolumns.value, ()=>{
+  //    return if(item.key == 'action')
+  // })
+  // let actionIndex = _.sortedLastIndexBy(dynamiccolumns.value, { "key": "action","title":"Action","dataIndex":"action" }, function(o) { return o.key; });
+  // console.log(actionIndex);
+  _.remove(dynamiccolumns.value,(item:ColumnItem)=>{
+    return item.key == 'action'
+  }) 
+  dynamiccolumns.value.push(newcolumn);
+  dynamiccolumns.value.push(actionColumn);
+};
+// _.remove(dynamiccolumns.value,(item:ColumnItem)=>{
+//     return item.key == key
+//   }) 
+
+sessionStorage.setItem('static_' + route.params._id, JSON.stringify(route.params._id))
+// sessionStorage.setItem('static_'+route.params.name,JSON.stringify(route.params.name))
+// 获取当前数据并赋值
+let recordobj = ref()
+// 根据传来的name值获取到数据
+async function query(data?: any) {
+  //  let rst=await request.get('/api/templates',{params:{q:'category:static', search:data}})
+  let rst = await request.get(`/api/templates/${data}`, { params: { q: 'category:static', search: '' } })
+  console.log(rst);
+  recordobj.value = rst
+  if (rst.model) {
+
+    recordobj.value.model = rst.model
+    tableData.value = arr(rst.model)
+      ;
+  } else {
+
+  }
+}
+// 给每条数据添加条属性
+const arr = (dataArr: any) => dataArr.map((item: any, index: string) => ({ ...item, key: index }))
+onMounted(() => {
+  let getId: any = sessionStorage.getItem('static_' + route.params._id)
+  console.log(JSON.parse(getId));
+
+  query(JSON.parse(getId))
+})
+const editableData: UnwrapRef<Record<string, TableDataItem>> = reactive({});
+
+// 修改static的方法
+const updstatic = async (data: any) => {
+
+  let rst = await request.put(`/api/templates/${recordobj.value._id}`, data)
+  message.success('Modification succeeded')
+}
+
+const edit = (key: string) => {
+  editableData[key] = cloneDeep(tableData.value.filter((item: { key: string; }) => key === item.key)[0]);
+  inputType.value = editableData[key].contacttype
+  // state.tags=editableData[key].enum
+};
+// 点击save触发的函数
+const save = async (obj: any) => {
+  // editableData[obj.key].enum=state.tags
+  Object.assign(tableData.value.filter((item: { key: string; }) => obj.key === item.key)[0], editableData[obj.key]);
+  //   delete editableData[obj.key].key
+  console.log(editableData[obj.key]);
+
+  recordobj.value.model = tableData.value
+  console.log(recordobj.value);
+
+  // await updstatic(recordobj.value)
+  // delete editableData[obj.key];
+
+}
+// 点击删除的方法
+const cancel = async (obj: any) => {
+  // delete tableData.value[tableData.value.indexOf(obj)]
+  tableData.value = tableData.value.filter((item: any) => item.enum !== obj.enum)
+  delete editableData[obj.key];
+  console.log(recordobj.value, tableData.value);
+  recordobj.value.model = tableData.value
+  let rst = await request.put(`/api/templates/${recordobj.value._id}`, recordobj.value)
+  console.log(rst);
+
+  message.success('test template has been deleted successfully')
+  query()
+};
+// 点击添加数据
+// const saveModel = () => {
+//   const newModel = {
+//     name: 'new Model name',
+//     type: 'new Model type',
+//     enum: ""
+//   }
+//   tableData.value.push({ ...newModel })
+// }
+// 定义属性判断输入框该输入的数据类型
+let inputType = ref()
+// 改变type的值
+const handleChange = (value: any) => {
+  inputType.value = value
+  console.log(inputType.value);
+
+};
+const changeType = (value: any) => {
+  if (inputType.value == "int") {
+    let reg = /^[0-9]+$/;
+    if (value != "" && !reg.test(value)) {
+      alert('只能输入整数！');
+      return false;
+    }
+  }
+  // if(inputType.value=='number'){
+  //   let type=typeof value
+  //   if(value!=""&& type!=='number'){
+  //     alert('只能输入数字')
+  //     return false
+  //   }
+  // }
+
+}
+
+// 获取新建tags的dom
+let inputRef = ref();
+// 添加的表单tags
+let state = reactive<statesTs>({
+  inputVisible: false,
+  inputValue: '',
+  tags: null
+});
+// 定义tag标签的输入类型
+interface statesTs {
+  tags: any
+  inputVisible: Boolean;
+  inputValue: string
+}
+// 点击添加标签的方法
+const showInput = () => {
+  state.inputVisible = true;
+  nextTick(() => {
+    inputRef.value.focus();
+  })
+};
+// 移除tags
+const handleClose = (removedTag: string) => {
+  const tags = state.tags.filter((tag: string) => tag !== removedTag);
+  state.tags = tags;
+};
+// 表格的数据结构
+
+function deleteCol(key: string) {
+
+  
+  _.remove(dynamiccolumns.value,(item:ColumnItem)=>{
+    return item.key == key
+  }) 
+
+}
+
+</script>
+
+<template>
+  <main style="height:100%;overflow-x: hidden!important;">
+    <div class="statictemplate">
+      <a-button type="primary" @click="columnshandleAdd()">
+        Add column
+      </a-button>
+    </div>
+    <div>
+      <a-table :dataSource="tableData" :columns="dynamiccolumns" >
+        <template #headerCell="{ column,text,record }" >
+         <template v-if="column.key !== 'action'">
+            <!-- <span>{{column.title}}</span> -->
+            <a-input v-if="editableData[column.key]" v-model:value="editableData[column.key].name"
+                  style="margin: -5px 0" />
+                <template v-else>
+                  {{column.title}}
+                </template>
+            <delete-outlined @click="deleteCol(column.key)"></delete-outlined>
+          </template> 
+
+          </template>
+          <template #bodyCell="{ column, text, record }">
+            <template v-if='column.key==="name"'>
+              <div>
+                <a-input v-if="editableData[record.key]" v-model:value="editableData[record.key].name"
+                  style="margin: -5px 0" />
+                <template v-else>
+                  {{text}}
+                </template>
+              </div>
+            </template>
+
+            <template v-else-if="column.dataIndex === 'action'">
+              <div class="editable-row-operations">
+                <span v-if="editableData[record.key]">
+                  <a-typography-link @click="save(record)">Save</a-typography-link>
+                  <a-popconfirm title="Sure to cancel?" @confirm="cancel(record)">
+                    <a style="margin-left:10px;font-size:14px">
+                      <delete-outlined />
+                    </a>
+                  </a-popconfirm>
+                </span>
+                <span v-else>
+                  <a @click="edit(record.key)">Edit</a>
+                </span>
+              </div>
+            </template>
+          </template>
+      </a-table>
+
+    </div>
+  </main>
+</template>
+
+<style scoped lang="less">
+.statictemplate {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+  align-content: flex-end;
+  align-items: flex-end;
+}
+
+.iconsave {
+  margin-left: 2.5rem;
+  width: 3.125rem !important;
+  font-size: 1.25rem !important;
+}
+
+/deep/ .ant-table-tbody {
+  >tr:hover:not(.ant-table-expanded-row)>td,
+  .ant-table-row-hover,
+  .ant-table-row-hover>td {
+    background: none !important;
+    //这里是将鼠标移入时的背景色取消掉了
+  }
+}
+</style>
