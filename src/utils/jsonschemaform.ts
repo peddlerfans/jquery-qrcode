db4a78bd6bd962f814c5790e8961b6a3bb5d7c3b
David <david.dai@iteatechnologies.com>
2022-10-19 23:50:42 +0800

    [MBT-100] json schema form layout and [MBT-104] dynamic params in aw shown

---
+++ src/utils/jsonschemaform.ts
@@ -0,0 +1,159 @@
+import { strHash } from './../../../mbt-backend/utils/hash_algorithm';
+// 输入array,生成jsonschemaform支持的样式
+//  [{
+//     "key": 0,
+//     "name": "testcase_id",
+//     "type": "str",
+//     "enum": "",
+//     "description": "用例编号"
+// }]
+
+// 输出：
+// [{
+//    "testcase_id": {
+//   "type": "string",
+//   "title": "用例编号"
+//    }]
+import * as _ from "lodash";
+import {ref,reactive} from 'vue';
+const generateSchema = (inputArr: Array<Object>) => {
+  
+  let outputArr: any[] = [];
+  inputArr.forEach((mod: any) => {
+    // let keyname = mod.name;
+    let typeinschema = "";
+    let enumVal: any[] = [];
+    switch (mod.type) {
+      case "int":
+        typeinschema = "number";
+        break;
+      case "str":
+        typeinschema = "string";
+        break;
+      case "float":
+        typeinschema = "number";
+        break;
+      case "number":
+        typeinschema = "number";
+        break;
+      case "boolean":
+        typeinschema = "boolean";
+        break;
+      case "SUT":
+        typeinschema = "string";
+        break;
+      default:
+        break;
+    }
+    let tempobj;
+    if (
+      mod.hasOwnProperty("enum") &&
+      mod.enum.length > 0 &&
+      typeinschema == "string"
+    ) {
+      enumVal = _.split(mod.enum, ",");
+      if (mod.description) {
+        tempobj = {
+          [mod.description]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title: mod.description,
+            enum: enumVal,
+            enumNames: enumVal,
+          },
+        };
+      } else if (mod.name) {
+        tempobj = {
+          [mod.name]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title: mod.name,
+            enum: enumVal,
+            enumNames: enumVal,
+          },
+        };
+      }
+    } else if (
+      mod.hasOwnProperty("enum") &&
+      mod.enum.length > 0 &&
+      typeinschema == "number"
+    ) {
+      let tempenumVal = _.split(mod.enum, ",");
+      let len = tempenumVal.length;
+      for (let i = 0; i < len; i++) {
+        enumVal.push(parseInt(tempenumVal[i]));
+      }
+      if(mod.description){
+        tempobj = {
+          [mod.name]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title: mod.description ,
+            enum: enumVal,
+            enumNames: enumVal,
+          },
+        };
+        
+      }else{
+        tempobj = {
+          [mod.name]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title:  mod.name,
+            enum: enumVal,
+            enumNames: enumVal,
+          },
+        };
+
+      }
+      
+    } else {
+      if(mod.description){
+        tempobj = {
+          [mod.name]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title: mod.description ,
+          }
+        }
+      }else {
+        tempobj = {
+          [mod.name]: {
+            type: `${typeinschema}`,
+            // description: mod.description,
+            title:  mod.name,
+          }
+        }
+      }
+      
+      
+    }
+    outputArr.push(tempobj);
+  });
+
+  return outputArr;
+};
+
+// let ss = ref(true)
+
+const generateObj = (refObj: any) => {
+  
+  let obj={};
+  if(typeof refObj=='object' && refObj.hasOwnProperty('_rawValue') && refObj.hasOwnProperty('_value')){
+    _.keys(refObj['_value']).forEach((k:string)=>{
+      
+      
+        // console.log('kkkkkkkk   ',refObj['_value'][k]);
+        let tempobj = {
+        [k]:refObj['_value'][k]
+        }
+        Object.assign(obj,tempobj);
+      
+
+    })
+    
+  }
+  
+  return obj;
+}
+export { generateSchema,generateObj };
