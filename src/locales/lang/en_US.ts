639cb8919819a2fd8b8dcd66ed7f123999ae0485
Peter Peng <peter.peng1215@gmail.com>
2022-11-04 18:58:24 +0000

    Feature international

--- src/locales/lang/en_US.ts
+++ src/locales/lang/en_US.ts
@@ -1,13 +1,21 @@
 import antdLocale from 'ant-design-vue/es/locale/en_US';
 import { genMessage } from '../helper';
+import common from "./en-US/common";
+import component from './en-US/component'
+import awModeler from "@/locales/lang/en-US/routes/awModeler";
+import MBTStore from '@/locales/lang/en-US/routes/MBTStore'
+import templateManager from "@/locales/lang/en-US/routes/templateManager";
+import layout from "@/locales/lang/en-US/layout";
 
 // const modulesFiles = require.context('./en-US', true, /\.ts$/);
 
 export default {
-  message: {
-    // ...genMessage(modulesFiles, 'en-US'),
-    antdLocale,
-  },
-  dateLocale: null,
-  dateLocaleName: 'en-US',
+  layout,
+  // ...genMessage(modulesFiles, 'en-US'),
+  antdLocale,
+  common,
+  component,
+  awModeler,
+  MBTStore,
+  templateManager
 };
--- src/views/mbtmodeler.vue
+++ src/views/mbtmodeler.vue
@@ -58,6 +58,9 @@ import { stringLiteral } from "@babel/types";
 import { array, func } from "vue-types";
 import CreateRule from "@/components/CreateRule.vue"
 import { propsToAttrMap } from "@vue/shared";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n()
 
 window.joint = joint;
 
@@ -445,7 +448,7 @@ const handleFinishExpected: FormProps["onFinish"] = (values: any) => {
  * Panel --Json schema forms
  */
  let tableDataDirectInput = ref([]);
- let 
+ let
  tableColumnsDirectInput = ref([])
 let globalformData = ref<Stores.mbtView>({
   _id: "",
@@ -569,7 +572,7 @@ const awschema = ref({
   },
 });
 let awschemaExpected = _.cloneDeep(awschema);
-  
+
 // linkData
 // "ui:hidden": "{{linkData.loop === false}}"
 const uischema={
@@ -610,7 +613,7 @@ const linkschema = ref({
       // "ui:hidden": "{{parentFormData.loop === true}}"
       // "ui:widget": CreateRule,
     },
-    
+
   },
 });
 
@@ -844,7 +847,7 @@ function awhandlerSubmit() {
   // console.log('new cacheprops:   ', cacheprops)
   currentElementMap.clear();
   onCloseDrawer();
-  message.success("Save aw Successfully");
+  message.success(t('component.message.saveSuccess'));
 }
 
 /**
@@ -857,7 +860,7 @@ function globalhandlerSubmit() {
   Object.assign(metaObj, { data: metatemplatedetailtableData.value });
   cacheDataDefinition.meta = metaObj;
   onCloseDrawer();
-  message.success("Save config Successfully");
+  message.success(t('component.message.saveSuccess'));
 }
 
 function linkhandlerSubmit() {
@@ -868,7 +871,7 @@ function linkhandlerSubmit() {
   }else{
     linkFormData.label!=undefined
   }
-  
+
   linkFormData.ruleData = rulesData.value;
   // linkFormData.loopcount = linkData.value.loopcount;
   linkFormData.connectorType = linkData.value.connectorType;
@@ -912,7 +915,7 @@ function linkhandlerSubmit() {
   Object.assign(tempObj, { routerType: linkFormData.routerType });
   cacheprops.set(lv_id, { props: tempObj });
   onCloseDrawer();
-  // message.success("Save it Successfully");
+  // message.success(t('component.message.saveSuccess'));
 }
 
 function handlerEditExpected() {
@@ -974,7 +977,7 @@ let toReload = ref(false);
   arr.forEach((item: any,index: any)=>{
   let keys=Object.keys(arr[0])
   if(keys){
-    
+
     setarr.forEach((tureitem: {type: string; name: string; values: any[];},i: any)=>{
       // console.log(keys[i]);
       if(tureitem.name==keys[i]){
@@ -1044,17 +1047,17 @@ async function mbtquery(id?: any, reLoad?: boolean) {
     // 后台请求数据地方
     rst = await request.get(url + "/" + id);
     console.log(rst);
-    
+
     if(rst && rst.dataDefinition && rst.dataDefinition.data){
       if(rst.dataDefinition?.data.tableColumns && rst.dataDefinition?.data.tableData ){
         // showAddConditional.value=true
       condataName.value=rst.dataDefinition?.data.tableColumns
       conditionalValue.value=rst.dataDefinition?.data.tableData
       console.log(condataName.value,conditionalValue.value);
-      
+
     }
     }
-    
+
     // condataName.value=rst.dataDefinition?.data.tableData
     if (rst && rst.name == route.params.name) {
       let str = rst._id + "";
@@ -1153,9 +1156,9 @@ async function saveMBT(route?: any) {
   // console.log("savembt meta and data:", cacheDataDefinition);
   mbtCache["dataDefinition"] = cacheDataDefinition;
   console.log(mbtCache);
-  
+
   await updateMBT(url + `/${mbtCache["_id"]}`, mbtCache);
-  message.success("Save MBT model successfully");
+  message.success(t('component.message.saveSuccess'));
 }
 
 function reloadMBT(route: any) {
@@ -1234,7 +1237,7 @@ function reloadMBT(route: any) {
       });
     }
   });
-  message.success("MBT model reloaded");
+  message.success(t('MBTStore.reloadTip'));
 }
 
 let dataFrom = ref('');
@@ -1345,15 +1348,15 @@ onMounted(() => {
    * Drag & Drop stencil to modeler paper
    */
   stencil.paper.on("cell:pointerdown", (cellView, e: dia.Event, x, y) => {
-    
-    
+
+
     let aw = "";
     let cellid = ""; //element ID
     $("body").append(
       '<div id="flyPaper" style="position:fixed;z-index:100;opacity:.7;pointer-event:none;"></div>'
     );
     let flyGraph = new joint.dia.Graph({ cellNamespace: namespace });
-    
+
     let flyPaper = new joint.dia.Paper({
       el: $("#flyPaper"),
       model: flyGraph,
@@ -1361,7 +1364,7 @@ onMounted(() => {
       cellViewNamespace: namespace,
     });
     let flyShape = cellView.model!.clone();
-    
+
     let pos = cellView.model!.position();
     let offset = {
       x: x - pos.x,
@@ -1417,8 +1420,8 @@ onMounted(() => {
   modeler.paper.on("link:pointerdblclick", async function  (linkView: any) {
     // 判断是否选择了模板，没选择则不打开link
     console.log(stencil,modeler);
-    
-    
+
+
     lv_id = linkView.model.id + "";
     // await queryName()
     isAW.value = false;
@@ -1428,12 +1431,12 @@ onMounted(() => {
       let templinkData = cacheprops.get(linkView.model.id);
       console.log(templinkData);
       linkData.value = templinkData.props;
-    
+
       if(templinkData.props.ruleData&&templinkData.props.ruleData.length>0){
         rulesData.value=templinkData.props.ruleData
       }
-      
-      
+
+
       if(linkData.value.editable){
 
       }
@@ -1834,8 +1837,8 @@ const handleDirectInput = (data:any) => {
 const handleStaticTable = (data:any) => {
   // console.log('get data from static children',data  )
   let tempObj = {};
-  
-  
+
+
   Object.assign(tempObj, { tableData: data.tableData });
   Object.assign(tempObj, { tableColumns: data.tableColumns });
   Object.assign(tempObj,{dataFrom:'static_template'})
@@ -1870,15 +1873,15 @@ let ifNameOpetions=computed(()=>{
 })
 let childvalue=computed(()=>{
   console.log(conditionalValue.value);
-  
+
   if(conditionalValue.value.length>0){
     // showAddConditional.value=true
     return valueOption(conditionalValue.value)
-    
+
   }else{
     // return showAddConditional.value=false
   }
-  
+
 })
 // 表格的数据
 let conditionalDatasource=ref<any>([])
@@ -1903,7 +1906,7 @@ const rulesData=ref(
   ]
 )
 const rulesChange=(datas: any,key:string)=>{
-    rulesData.value=datas//输出的条件对象 
+    rulesData.value=datas//输出的条件对象
 }
 // 递归改变if结构
 function ifdata(arr:any){
@@ -1922,13 +1925,13 @@ function ifdata(arr:any){
       // finditem=conditionstr(item.conditions)+' '+item.relation
       finditem=`${conditionstr(item.conditions)} ${item.relation} `
     }
-    if(item.children.length>0){      
+    if(item.children.length>0){
       finditem+=ifdata(item.children)
       // if
     }else{
       break
     }
-    
+
   }
   if(finditem!=null){
     let findlength=finditem.length
@@ -1938,7 +1941,7 @@ function ifdata(arr:any){
 
     return finditem
   }
-  
+
 }
 function selectvalue(value:any){
   let values=null
@@ -1970,7 +1973,7 @@ function selectvalue(value:any){
 const conditionstr=(arr:any)=>{
   let ifcondition=null
   // if(arr[arr.length-1].value){
-        // delete arr[arr.length-1].selectvalues  
+        // delete arr[arr.length-1].selectvalues
   // }
   ifcondition=arr.map((item:any)=>{
     if(item.operator=='='){item.operator="=="}
@@ -1982,10 +1985,10 @@ const conditionstr=(arr:any)=>{
       }else{
         // return '['+item.name+']'+' '+item.operator+' '+'{'+item.value+'}'+' '
         return `${item.name} ${item.operator} ${JSON.stringify(item.value)} `
-      }    
+      }
   })
- 
-  
+
+
   return ifcondition.join("").toString().substring(0,ifcondition.join("").toString().length-4)
 }
 watch(rulesData,(newvalue:any)=>{
@@ -2005,9 +2008,9 @@ watch(rulesData,(newvalue:any)=>{
       <a-row>
         <a-col span="18">
           <a-button-group>
-            <a-button type="primary" @click="saveMBT(route)"> Save </a-button>
+            <a-button type="primary" @click="saveMBT(route)"> {{ $t('common.saveText') }} </a-button>
             <span style="margin-left: 5px">
-              <a-button danger @click="reloadMBT(route)"> Reload </a-button>
+              <a-button danger @click="reloadMBT(route)"> {{ $t('layout.multipleTab.reload') }} </a-button>
             </span>
           </a-button-group>
         </a-col>
@@ -2191,7 +2194,7 @@ watch(rulesData,(newvalue:any)=>{
                         >
                       </span>
                       <span style="margin-right: 5px">
-                        <a-button type="primary" @click="handlerCancel()">Edit</a-button>
+                        <a-button type="primary" @click="handlerCancel()">{{ $t('common.editText') }}</a-button>
                       </span>
                       <a-button danger @click="onExpectedAW()">Next</a-button>
                     </div>
@@ -2313,7 +2316,7 @@ watch(rulesData,(newvalue:any)=>{
                     <div slot-scope="{ awformdataExpected }">
                       <span style="margin-right: 5px">
                         <a-button type="primary" @click="handlerEditExpected()"
-                          >Edit</a-button
+                          >{{ $t('common.editText') }}</a-button
                         >
                       </span>
                       <span style="margin-right: 5px">
@@ -2324,8 +2327,8 @@ watch(rulesData,(newvalue:any)=>{
                       <span style="margin-left: 5px">
                         <a-popconfirm
                           title="Are you sure clear this form?"
-                          ok-text="Yes"
-                          cancel-text="No"
+                          :ok-text="$t('common.yesText')"
+                          :cancel-text="$t('common.noText')"
                           @confirm="handlerClearExpected()"
                           @cancel="cancel"
                         >
@@ -2357,12 +2360,12 @@ watch(rulesData,(newvalue:any)=>{
               <a-button @click="onCloseDrawer">cancel</a-button>
               </div>
               </div>
-             
-              
-              
+
+
+
             </VueForm>
               <!-- <div v-if="showAddFactorBtn=false"> -->
-              
+
             <!-- </div> -->
 
             </div>
@@ -2479,17 +2482,17 @@ watch(rulesData,(newvalue:any)=>{
                       <div class="editable-row-operations">
                         <span v-if="resourceseditableData[record.key]">
                           <a-typography-link @click="resourcessave(record.key)"
-                            >Save</a-typography-link
+                            >{{ $t('common.saveText') }}</a-typography-link
                           >
                           <a-popconfirm
-                            title="Sure to cancel?"
+                            :title="$t('component.message.sureCancel')"
                             @confirm="resourcescancel(record.key)"
                           >
-                            <a>Cancel</a>
+                            <a>{{ $t('common.cancelText') }}</a>
                           </a-popconfirm>
                         </span>
                         <span v-else>
-                          <a @click="resourcesedit(record.key)">Edit</a>
+                          <a @click="resourcesedit(record.key)">{{ $t('common.editText') }}</a>
                         </span>
                         <span>
                           <a-popconfirm
@@ -2497,14 +2500,14 @@ watch(rulesData,(newvalue:any)=>{
                             title="Sure to delete?"
                             @confirm="onresourcesDelete(record.key)"
                           >
-                            <a> Delete</a>
+                            <a> {{ $t('common.delText') }}</a>
                           </a-popconfirm>
                         </span>
                       </div>
                     </template>
                   </template>
                 </a-table>
-                <a-button type="primary" @click="globalhandlerSubmit">Save</a-button>
+                <a-button type="primary" @click="globalhandlerSubmit">{{ $t('common.saveText') }}</a-button>
               </a-tab-pane>
             </a-tabs>
           </div>
@@ -2624,7 +2627,7 @@ header {
         color: red !important;
       }
     }
-    
+
   }
   .ant-form-item-control{
     .ant-form-item-control-input{
@@ -2648,7 +2651,7 @@ header {
         color: red !important;
       }
     }
-    
+
   }
 }
 .__pathRoot_template{
@@ -2658,7 +2661,7 @@ header {
         color: red !important;
       }
     }
-    
+
   }
 }
 .__pathRoot_tags{
@@ -2668,7 +2671,7 @@ header {
         color: red !important;
       }
     }
-    
+
   }
 }
 </style>
