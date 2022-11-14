eaa9f2d94e78cfd7918f13fcfc5ff920c293dd68
fans <Fzh0128@126.com>
2022-11-14 12:26:42 +0800

    optimization

--- components.d.ts
+++ components.d.ts
@@ -39,7 +39,6 @@ declare module '@vue/runtime-core' {
     APopconfirm: typeof import('ant-design-vue/es')['Popconfirm']
     ARadio: typeof import('ant-design-vue/es')['Radio']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
-    AResult: typeof import('ant-design-vue/es')['Result']
     ARow: typeof import('ant-design-vue/es')['Row']
     ASelect: typeof import('ant-design-vue/es')['Select']
     ASlider: typeof import('ant-design-vue/es')['Slider']
