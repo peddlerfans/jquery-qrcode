408378a775a0cc1d3fa710157247bcada2be0074
Peter Peng <peter.peng1215@gmail.com>
2022-11-15 14:15:00 -0600

    remove unfinished testcases

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -36,37 +36,37 @@ await delay(1000);
 
 );
 
-test('Failed Login', async ({ page }) => {
-  await page.goto('http://localhost:7777/');
+// test('Failed Login', async ({ page }) => {
+//   await page.goto('http://localhost:7777/');
 
-  await page.waitForSelector('#loginForm_username');
-  await page.fill('#loginForm_username',"david");
-  await page.fill('#loginForm_password',"abcd");
-  await Promise.all([
-        page.click('button[type=submit]'),
-        page.waitForNavigation({waitUntil:'networkidle', timeout:2000})]);
+//   await page.waitForSelector('#loginForm_username');
+//   await page.fill('#loginForm_username',"david");
+//   await page.fill('#loginForm_password',"abcd");
+//   await Promise.all([
+//         page.click('button[type=submit]'),
+//         page.waitForNavigation({waitUntil:'networkidle', timeout:2000})]);
 
-  expect(await page.url(), "should navigate to dahsboard page").toMatch(/dashboard$/);
-  await Promise.all([    page.click('*[data-menu-id="/awmodeler/index"] a'),
-     page.waitForNavigation({waitUntil:'networkidle', timeout:2000})]);
-await delay(1000);
-     console.log(await page.url())
-  //  try {
-  //    await page.click('*[data-menu-id="/awmodeler/index"] a');
-  //    await page.waitForNavigation({timeout:5000});
-  //  } catch (e) {
+//   expect(await page.url(), "should navigate to dahsboard page").toMatch(/dashboard$/);
+//   await Promise.all([    page.click('*[data-menu-id="/awmodeler/index"] a'),
+//      page.waitForNavigation({waitUntil:'networkidle', timeout:2000})]);
+// await delay(1000);
+//      console.log(await page.url())
+//   //  try {
+//   //    await page.click('*[data-menu-id="/awmodeler/index"] a');
+//   //    await page.waitForNavigation({timeout:5000});
+//   //  } catch (e) {
 
-  //  }
- await page.screenshot({path:"./test.png"});
-// expect(await page.url(), "should navigate to awmodeler page").toMatch(/awmodeler/);
+//   //  }
+//  await page.screenshot({path:"./test.png"});
+// // expect(await page.url(), "should navigate to awmodeler page").toMatch(/awmodeler/);
 
-   console.log(await page.url())
-   const pageTitle = await page.title();
+//    console.log(await page.url())
+//    const pageTitle = await page.title();
 
 
-},
+// },
 
-);
+// );
 // AW管理
 // 账户管理
 // group
