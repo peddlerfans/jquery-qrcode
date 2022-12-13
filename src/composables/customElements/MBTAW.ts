ab568c3214a7291867ae618fb4796f917b50e9a5
David <david.dai@iteatechnologies.com>
2022-12-13 12:16:32 -0600

    Remove vitest ref in MBT Elements

--- src/composables/customElements/MBTAW.ts
+++ src/composables/customElements/MBTAW.ts
@@ -1,10 +1,8 @@
-import { ConsoleSqlOutlined } from "@ant-design/icons-vue";
-// import * as joint from "jointjs";
 import {MBTShapeInterface} from "./MBTShapeInterface"
 import joint from "../../../node_modules/@clientio/rappid/rappid.js"
 // import * as joint from '@clientio/rappid';
 const { dia, g,ui,shapes } = joint
-console.log('shapes xxxxx:',shapes)
+
 import { i18n } from "@/locales";
 import { MBTStore } from "@/stores/MBTModel"
 const store = MBTStore()
