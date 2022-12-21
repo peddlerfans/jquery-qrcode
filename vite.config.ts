f23a517a4562955a1e6647afe515b13ec604783c
David <david.dai@iteatechnologies.com>
2022-12-20 21:10:20 -0600

    Solve the issue vite commonjs plugin for joint js cause side effect to ace-builds

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,9 @@ export default defineConfig({
   plugins: [
     vue(),
     DefineOptions(),
-    viteCommonjs(),
+    viteCommonjs(
+      {exclude:['ace-builds']}
+      ),
     // viteMockServe({
     //   supportTs: true, 
     // }),
